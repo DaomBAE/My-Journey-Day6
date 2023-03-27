2edf13202c80c7362f7130e8e9c5e7cd96d743ec
DaomBae <da6970@gmail.com>
2023-03-27 15:10:10 +0900

    homepage

--- main.js
+++ main.js
@@ -1,15 +1,114 @@
+const QUOTES = "quotes";
+
 function getTime() {
-    const time = document.querySelector(".time");
-  
-    const newDate = new Date();
-  
-    const hours = String(newDate.getHours()).padStart(2, "0");
-    const minutes = String(newDate.getMinutes()).padStart(2, "0");
-    const seconds = String(newDate.getSeconds()).padStart(2, "0");
-  
-    // time.innerText = hours + ":" + minutes + ":" + seconds;
-    time.innerText = `${hours}:${minutes}:${seconds}`;
+  const time = document.querySelector(".time");
+
+  const newDate = new Date();
+
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getSeconds()).padStart(2, "0");
+
+  // time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
+
+function getQuotes() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES,
+      JSON.stringify([
+        "Hey, You Can Survive!",
+        "Right Under Your Nose",
+        "Find V Restaurant Near You",
+        "Whatever",
+      ])
+    );
+
+    savedQuotes = localStorage.getItem(QUOTES);
   }
+
+  let quotesArray = JSON.parse(savedQuotes);
+
+  quotesMsg.innerText =
+    quotesArray[Math.floor(Math.random() * quotesArray.length)];
+}
+
+getQuotes();
+
+function onClickAdd() {
+  const newQuotes = document.querySelector(".newQuotes");
+
+  newQuotes.style.display = "inline-block";
+}
+
+function onClickRegist() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotes = document.querySelector(".newQuotes");
+  const newQuotesInput = document.querySelector(".newQuotesInput");
+
+  if (!newQuotesInput.value) {
+    return;
+  }
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  let quotesArray = JSON.parse(savedQuotes);
+  quotesArray.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
+
+  quotesMsg.innerHTML = `<span style="color:red;">${newQuotesInput.value}</span>`;
+  newQuotes.style.display = "none";
+  newQuotesInput.value = "";
+}
+
+  // const a = {
+  //   question: "질문입니다.",
+  // };
+
+  // const b = {
+  //   question: question,
+  // };
+
+  // const c = {
+  //   question,
+  // };
+
+async function onClickSearch() {
+  const searchInput = document.querySelector(".searchInput");
+  const searchResult = document.querySelector(".searchResult");
+
+  if (!searchInput.value) return;
   
-  getTime();
-  setInterval(getTime, 1000);
+  const question = searchInput.value;
+
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요.";
+
+  //  프론트에서 백앤드로 보내는 코드
+  const response = await axios.post(
+    "https://holy-fire-2749.fly.dev/chat",
+    {
+      question,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
+      },
+    }
+  );
+
+  if (response.status === 200) {
+    searchResult.style.display = "inline";
+    searchResult.innerText = response.data.choices[0].message.content;
+  }
+
+  searchInput.value = "";
+}
